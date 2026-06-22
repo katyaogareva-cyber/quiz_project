4cb5da24bc38fa1ae4e9e41c34d19727be2514da
site <katyaogareva@gmail.com>
2026-06-22 20:10:40 +0300

    Delete core/mock_permissions.py

--- core/mock_permissions.py
+++
@@ -1,4 +0,0 @@
-def has_permission(role, element, action):
-    role_permissions = PERMISSIONS.get(role, {})
-    allowed_actions = role_permissions.get(element, [])
-    return action in allowed_actions
